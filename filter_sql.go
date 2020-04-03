d200ba3c7beae8c49204a57572c1a67a947a9edd
rui.xu <rui.xu@liulishuo.com>
2020-04-03 15:39:04 +0800

    1.fix bug: pongo2 模板类的 list 参数需要用 反射特别处理

--- filter_sql.go
+++ filter_sql.go
@@ -1,19 +1,31 @@
 package pongo2
 
 import (
+	"fmt"
+	"reflect"
 	"strings"
 )
 
+type NotListError struct {
+}
+
+func (v *NotListError) Error() string {
+	return "Not List! Please check it when use pongo2 template\n"
+}
+
 func filterExpandListInt(in *Value, param *Value) (*Value, *Error) {
 
-	listArgs, err := in.([]int)
-	listStr := make([]string, 0)
+	str := make([]string, 0)
 
-	if err != nil {
+	if in.getResolvedValue().Kind() == reflect.Slice {
+		items := in.getResolvedValue()
+		for i := 0; i < items.Len(); i++ {
+			item := items.Index(i)
+			str = append(str, fmt.Sprintf("%v", item))
+		}
+	} else {
+		err := &NotListError{}
 		return nil, err
-	}
-	for i, _ := range listArgs {
-		listStr = append(listStr, listArgs[i])
-	}
-	return AsValue(strings.Join(listStr, ", ")), nil
+
+	return AsValue(strings.Join(str, ",")), nil
 }
